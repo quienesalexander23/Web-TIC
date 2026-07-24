65c6a5d7bd7b45962b292b2357aa1457af997825
Alexander Tibanta Miranda <alexandertibanta1@gmail.com>
2026-07-24 06:24:19 -0500

    Update environment.prod.ts

--- webtic-frontend-app/src/environments/environment.prod.ts
+++ webtic-frontend-app/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // Reemplaza esto con la URL real de tu backend en Render
-  // (ej. https://webtic-backend-api.onrender.com) antes de desplegar a Vercel.
-  apiUrl: 'https://TU-BACKEND.onrender.com'
+  apiUrl: 'https://web-tic.onrender.com'
 };
